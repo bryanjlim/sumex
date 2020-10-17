d41529dd856386bb37b4e0287723e732df072d7c
Bryan Lim <bryanlim@cs.washington.edu>
2020-10-17 15:59:18 -0700

    Use raw body text instead of JSON

--- functions/basic_textrank_summarizer/__init__.py
+++ functions/basic_textrank_summarizer/__init__.py
@@ -68,7 +68,7 @@ def generate_summary(sentences):
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    text = req.get_json()["text"]
+    text = req.get_body()
     sentences = text.split(". ")
     ret = " ".join(generate_summary(sentences)) if len(sentences) > 3 else text
     return func.HttpResponse(ret)
