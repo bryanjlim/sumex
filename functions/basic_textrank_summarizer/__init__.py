20a14f8b5d76f5adf1d7a9c699fd1bc31458fdd3
Bryan Lim <bryanlim@cs.washington.edu>
2020-10-17 15:45:33 -0700

    Cache English stopwords

--- functions/basic_textrank_summarizer/__init__.py
+++ functions/basic_textrank_summarizer/__init__.py
@@ -8,9 +8,7 @@ from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
 
-import nltk
-nltk.download('stopwords')
-
+english_stopwords = ["i","me","my","myself","we","our","ours","ourselves","you","you're","you've","you'll","you'd","your","yours","yourself","yourselves","he","him","his","himself","she","she's","her","hers","herself","it","it's","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","that'll","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","don't","should","should've","now","d","ll","m","o","re","ve","y","ain","aren","aren't","couldn","couldn't","didn","didn't","doesn","doesn't","hadn","hadn't","hasn","hasn't","haven","haven't","isn","isn't","ma","mightn","mightn't","mustn","mustn't","needn","needn't","shan","shan't","shouldn","shouldn't","wasn","wasn't","weren","weren't","won","won't","wouldn","wouldn't"]
 
 def sentence_similarity(sent1, sent2, stopwords=None):
     if stopwords is None:
@@ -55,7 +53,7 @@ def build_similarity_matrix(sentences, stop_words):
 
 def generate_summary(sentences):
     top_n = int(math.log(len(sentences), 2))
-    stop_words = stopwords.words('english')
+    stop_words = english_stopwords
     summarize_text = []
     sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
     sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
@@ -76,3 +74,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     sentences = text.split(". ")
     ret = " ".join(generate_summary(sentences)) if len(sentences) > 3 else text
     return func.HttpResponse(ret)
+
