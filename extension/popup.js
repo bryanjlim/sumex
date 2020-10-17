a2a4308239f6e77ef037ceb56b9eb94366dbe98e
Farhan Saeed <farhansaeed1999@gmail.com>
2020-10-17 16:38:44 -0700

    extension modifies all ptags and inserts a thing at top of page

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,27 @@
-alert('hello')
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    var checkPageButton = document.getElementById("summ");
+    checkPageButton.addEventListener(
+      "click",
+      function() {
+        chrome.tabs.executeScript(
+          null,
+          {
+            file: "getPagesSource.js"
+          },
+          function() {
+            // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+            if (chrome.runtime.lastError) {
+              message.innerText =
+                "There was an error injecting script : \n" +
+                chrome.runtime.lastError.message;
+            }
+          }
+        );
+      },
+      false
+    );
+  },
+  false
+);
