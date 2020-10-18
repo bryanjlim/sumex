5c2579858ab8c73d901f816ae42ee0169e74b537
missingno01 <rayaltenberg@gmail.com>
2020-10-17 22:51:54 -0700

    Added TTS

--- extension/getPagesSource.js
+++ extension/getPagesSource.js
@@ -75,7 +75,7 @@ async function DOMtoString(document_root) {
 // edmundson - https://basic-text-summarizer.azurewebsites.net/api/edmundson_summarizer?code=DiDWZbUFgtEl7SZ0jCPaKWI95JSJY72lIxv53vya7TEDzfHScLfjaw==
 async function summarizer(text) {
     // Default options are marked with *
-    url = 'https://basic-text-summarizer.azurewebsites.net/api/edmundson_summarizer?code=DiDWZbUFgtEl7SZ0jCPaKWI95JSJY72lIxv53vya7TEDzfHScLfjaw=='
+    url = 'https://basic-text-summarizer.azurewebsites.net/api/basic_textrank_summarizer?code=vTb0P/llWsXcTiqclPcjR6PtN4wbOFMXtiZwUIaj0uUBTJX8i0WxYw=='
     const response = await fetch(url, {
         method: 'POST',
         cache: 'no-cache',
