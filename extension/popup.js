f1130ade7efbfb14d0e69705e0c38915e1a2127f
missingno01 <rayaltenberg@gmail.com>
2020-10-17 20:02:24 -0700

    Initial right Click Functionality

--- extension/popup.js
+++ extension/popup.js
@@ -26,8 +26,27 @@ document.addEventListener(
   false
 );
 
-// chrome.contextMenus.create({
-//   title: "Summarize with Sumex",
-//   contexts:["selection"],  // ContextType
-//   onclick: searchUrbanDict // A callback function
-//  });
+chrome.contextMenus.create({
+  title: "Summarize with Sumex",
+  contexts:["selection"],  // ContextType
+  onclick: summarizer // A callback function
+});
+
+
+
+async function summarizer(word) {
+  alert('TESTTESTTESTTES');
+  // Default options are marked with *
+  var query = word.selectionText;
+  url = 'https://basic-text-summarizer.azurewebsites.net/api/basic_textrank_summarizer?code=vTb0P/llWsXcTiqclPcjR6PtN4wbOFMXtiZwUIaj0uUBTJX8i0WxYw=='
+  const response = await fetch(url, {
+      method: 'POST',
+      cache: 'no-cache',
+      mode: 'cors',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: query
+  });
+  alert(response.text()); // parses JSON response into native JavaScript objects
+}
