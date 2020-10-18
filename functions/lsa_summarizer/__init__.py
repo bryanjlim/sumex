27f30878e84a1ed7441ffdc111f2204d70be0675
Drew Ehrlich <deehrlic@ucsc.edu>
2020-10-17 18:47:29 -0700

    added edmundson and lsa

--- functions/lsa_summarizer/__init__.py
+++ functions/lsa_summarizer/__init__.py
@@ -5,7 +5,7 @@ from __future__ import division, print_function, unicode_literals
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.luhn import LuhnSummarizer as Summarizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
